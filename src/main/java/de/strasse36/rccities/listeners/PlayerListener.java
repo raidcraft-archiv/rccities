896960ed449f71276f9910bbe7571760ca2b9993
Strasse36 <urban.philip@googlemail.com>
2012-03-13 17:20:34 +0100

    solved water and lava bug

--- src/main/java/de/strasse36/rccities/listeners/PlayerListener.java
+++ src/main/java/de/strasse36/rccities/listeners/PlayerListener.java
@@ -1,6 +1,12 @@
 package de.strasse36.rccities.listeners;
 
+import com.silthus.raidcraft.util.RCMessaging;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import de.strasse36.rccities.util.WorldGuardManager;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
  * Author: Philip Urban
@@ -9,4 +15,26 @@ import org.bukkit.event.Listener;
  */
 public class PlayerListener implements Listener
 {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event)
+    {
+        //check if cancelled
+        if(event.isCancelled())
+            return;
+
+        //check if location is region
+        ApplicableRegionSet regionSet = WorldGuardManager.getLocalRegions(event.getClickedBlock().getLocation());
+        if(regionSet.size() != 0)
+            return;
+
+        if(event.getPlayer().hasPermission("rccities.build.place"))
+            return;
+
+
+        if(event.getItem().getType() != Material.WATER_BUCKET && event.getItem().getType() != Material.LAVA_BUCKET)
+            return;
+
+        event.setCancelled(true);
+        RCMessaging.warn(event.getPlayer(), "Du hast hier keine Baurechte!");
+    }
 }
