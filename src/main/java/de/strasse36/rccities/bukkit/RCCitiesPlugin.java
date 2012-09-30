b7d88a7b1ae12abbe7ccabeac691f8a963d1607b
Philip Urban <urban.philip@googlemail.com>
2012-09-30 13:44:01 +0200

    add groups at startup

--- src/main/java/de/strasse36/rccities/bukkit/RCCitiesPlugin.java
+++ src/main/java/de/strasse36/rccities/bukkit/RCCitiesPlugin.java
@@ -1,12 +1,14 @@
 package de.strasse36.rccities.bukkit;
 
 import com.silthus.raidcraft.bukkit.BukkitBasePlugin;
+import de.strasse36.rccities.Resident;
 import de.strasse36.rccities.commands.PlotCommandAllocater;
 import de.strasse36.rccities.commands.TownCommandAllocater;
 import de.strasse36.rccities.config.MainConfig;
 import de.strasse36.rccities.database.RCCitiesDatabase;
 import de.strasse36.rccities.listeners.BlockListener;
 import de.strasse36.rccities.listeners.PlayerListener;
+import de.strasse36.rccities.util.PermissionsManager;
 import de.strasse36.rccities.util.TableHandler;
 import de.strasse36.rccities.util.Taxes;
 import org.bukkit.Bukkit;
@@ -29,6 +31,10 @@ public class RCCitiesPlugin extends BukkitBasePlugin {
         new MainConfig(this);
         TableHandler.init();
         Taxes.init();
+        
+        for(Resident resident : TableHandler.get().getResidentTable().getResidents()) {
+            PermissionsManager.addGroup(resident.getName(), resident.getCity().getName());
+        }
     }
 
     public static RCCitiesPlugin get()
