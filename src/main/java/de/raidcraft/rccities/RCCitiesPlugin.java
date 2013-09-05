ed9266de21827ad31e338c458d2ed59c6f03ee5a
Philip Urban <urban.philip@googlemail.com>
2013-09-05 14:43:59 +0200

    started work on RCCities v2

---
+++ src/main/java/de/raidcraft/rccities/RCCitiesPlugin.java
@@ -0,0 +1,18 @@
+package de.raidcraft.rccities;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Philip Urban
+ */
+public class RCCitiesPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+    }
+}
