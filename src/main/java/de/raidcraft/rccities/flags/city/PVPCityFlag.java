7b82f386a867058a456dd79a91cba0c70ebd1019
Philip Urban <urban.philip@googlemail.com>
2013-09-17 04:16:30 +0200

    expanded demo flag

--- src/main/java/de/raidcraft/rccities/flags/city/PVPCityFlag.java
+++ src/main/java/de/raidcraft/rccities/flags/city/PVPCityFlag.java
@@ -3,6 +3,7 @@ package de.raidcraft.rccities.flags.city;
 import de.raidcraft.rccities.api.city.City;
 import de.raidcraft.rccities.api.flags.AbstractCityFlag;
 import de.raidcraft.rccities.api.flags.FlagInformation;
+import de.raidcraft.rccities.api.flags.FlagRefreshType;
 import de.raidcraft.rccities.api.flags.FlagType;
 
 /**
@@ -10,7 +11,9 @@ import de.raidcraft.rccities.api.flags.FlagType;
  */
 @FlagInformation(
         name = "PVP",
-        type = FlagType.BOOLEAN
+        type = FlagType.BOOLEAN,
+        refreshType = FlagRefreshType.ON_CHANGE,
+        refreshInterval = 0
 )
 public class PVPCityFlag extends AbstractCityFlag {
 
