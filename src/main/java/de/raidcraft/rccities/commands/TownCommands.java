fe9f18448f4d26660d9cc183e8150025b9a38032
Philip Urban <urban.philip@googlemail.com>
2013-10-03 18:45:27 +0200

    fixed chat msg

--- src/main/java/de/raidcraft/rccities/commands/TownCommands.java
+++ src/main/java/de/raidcraft/rccities/commands/TownCommands.java
@@ -214,7 +214,7 @@ public class TownCommands {
             sender.sendMessage(ChatColor.GREEN + "Die Gilde '" + city.getFriendlyName() + "' hat das Upgrade '"
                     + ChatColor.YELLOW + upgradeRequest.getUpgradeLevel().getName() + ChatColor.GREEN + " beantragt:");
             sender.sendMessage(ChatColor.GREEN + "Info: " + ChatColor.GRAY + upgradeRequest.getInfo());
-            sender.sendMessage(ChatColor.GREEN + "-->" + ChatColor.YELLOW + "/gilde upgrades " + city.getName() + " <accept/decline>");
+            sender.sendMessage(ChatColor.GREEN + "-->" + ChatColor.YELLOW + "/gilde upgrades " + city.getName() + " <accept/reject>");
         }
 
         @Command(
