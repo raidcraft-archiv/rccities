4fc0a380fcd24f323134d9215c00fdf03cd48e46
Philip Urban <urban.philip@googlemail.com>
2013-11-01 17:03:01 +0100

    hopefully fixed performance issues with exp gaining

--- src/main/java/de/raidcraft/rccities/listener/ExpListener.java
+++ src/main/java/de/raidcraft/rccities/listener/ExpListener.java
@@ -3,18 +3,29 @@ package de.raidcraft.rccities.listener;
 import de.raidcraft.RaidCraft;
 import de.raidcraft.api.events.RCPlayerGainExpEvent;
 import de.raidcraft.rccities.RCCitiesPlugin;
+import de.raidcraft.rccities.api.city.City;
 import de.raidcraft.rccities.api.resident.Resident;
 import de.raidcraft.rccities.api.resident.Role;
 import de.raidcraft.rccities.api.resident.RolePermission;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Philip Urban
  */
 public class ExpListener implements Listener {
+
+    private static ExpSaver expSaverTask = new ExpSaver();
+
+    static {
+
+        Bukkit.getScheduler().runTaskTimer(RaidCraft.getComponent(RCCitiesPlugin.class), expSaverTask, 60 * 20, 60 * 20);
+    }
 
     @EventHandler(ignoreCancelled = true)
     public void onExpGain(RCPlayerGainExpEvent event) {
@@ -36,8 +47,28 @@ public class ExpListener implements Listener {
             }
         }
         if(resident != null) {
-            resident.getCity().addExp(event.getGainedExp());
+            expSaverTask.addExp(resident.getCity(), event.getGainedExp());
         }
     }
 
+    public static class ExpSaver implements Runnable {
+
+        private Map<City, Integer> gainedExp = new HashMap<>();
+
+        @Override
+        public void run() {
+
+            for(Map.Entry<City, Integer> entry : new HashMap<>(gainedExp).entrySet()) {
+
+                entry.getKey().addExp(entry.getValue());
+                gainedExp.remove(entry.getKey());
+            }
+        }
+
+        public void addExp(City city, int exp) {
+
+            if(!gainedExp.containsKey(city)) gainedExp.put(city, 0);
+            gainedExp.put(city, gainedExp.get(city) + exp);
+        }
+    }
 }
