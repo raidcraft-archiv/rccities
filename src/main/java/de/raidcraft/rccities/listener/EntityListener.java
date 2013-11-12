4d65115eab21c0c640c4b2d2c6388c1e3c5f7691
Philip Urban <urban.philip@googlemail.com>
2013-11-12 15:36:44 +0100

    performance improvement

--- src/main/java/de/raidcraft/rccities/listener/EntityListener.java
+++ src/main/java/de/raidcraft/rccities/listener/EntityListener.java
@@ -5,6 +5,7 @@ import de.raidcraft.rccities.RCCitiesPlugin;
 import de.raidcraft.rcconversations.RCConversationsPlugin;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
@@ -13,11 +14,11 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
  */
 public class EntityListener implements Listener {
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onEntitySpwan(CreatureSpawnEvent event) {
 
-        if(RaidCraft.getComponent(RCConversationsPlugin.class).getCitizens().getNPCRegistry().isNPC(event.getEntity())) return;
         if(!(event.getEntity() instanceof Monster)) return;
+        if(RaidCraft.getComponent(RCConversationsPlugin.class).getCitizens().getNPCRegistry().isNPC(event.getEntity())) return;
         if(RaidCraft.getComponent(RCCitiesPlugin.class).getPlotManager().getPlot(event.getEntity().getLocation().getChunk()) == null) return;
 
         event.getEntity().remove();
