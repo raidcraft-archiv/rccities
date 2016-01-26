19aacb1c78670e9af00b534f0ddfc71ccc74ad27
Philip <urban.philip@gmail.com>
2016-01-26 20:00:36 +0100

    allowe snowman, irongolem and wither

--- src/main/java/de/raidcraft/rccities/listener/EntityListener.java
+++ src/main/java/de/raidcraft/rccities/listener/EntityListener.java
@@ -2,7 +2,10 @@ package de.raidcraft.rccities.listener;
 
 import de.raidcraft.RaidCraft;
 import de.raidcraft.rccities.RCCitiesPlugin;
+import org.bukkit.entity.IronGolem;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.Snowman;
+import org.bukkit.entity.Wither;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,6 +20,8 @@ public class EntityListener implements Listener {
     public void onEntitySpwan(CreatureSpawnEvent event) {
 
         if (!(event.getEntity() instanceof Monster)) return;
+        // Allow snowman, irongolems, whitter
+        if((event.getEntity() instanceof IronGolem) || (event.getEntity() instanceof Snowman) || (event.getEntity() instanceof Wither)) return;
         // allow CUSTOM spawns, e.g. NPCs
         if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CUSTOM) return;
         if (RaidCraft.getComponent(RCCitiesPlugin.class).getPlotManager().getPlot(event.getEntity().getLocation().getChunk()) == null) {
